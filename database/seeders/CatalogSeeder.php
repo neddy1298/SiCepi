3534d9e866f58d48ad9deb5b2dfde5af2d3b8043
Neddy <neddy1298@gmail.com>
2021-01-02 10:26:49 +0700

    Categori Dummy

--- database/seeders/CatalogSeeder.php
+++ database/seeders/CatalogSeeder.php
@@ -13,10 +13,43 @@ class CatalogSeeder extends Seeder
      */
     public function run()
     {
-        $sales = [
-            'catalog' => 'Sales Letter'
-        ];
+        $sales = [[
+            'catalog' => 'Sales Letter',
+            'description' => 'Sales Letter'
+        ],[
+            'catalog' => 'Call To Action',
+            'description' => 'Call To Action'
+        ],[
+            'catalog' => 'Facebokk Ads',
+            'description' => 'Facebokk Ads'
+        ],[
+            'catalog' => 'Google AdWords Ads',
+            'description' => 'Google AdWords Ads'
+        ],[
+            'catalog' => 'Email Campaign',
+            'description' => 'Email Campaign'
+        ],[
+            'catalog' => 'Headlines',
+            'description' => 'Headlines'
+        ],[
+            'catalog' => 'Webinars',
+            'description' => 'Webinars'
+        ],[
+            'catalog' => 'Upsells',
+            'description' => 'Upsells'
+        ],[
+            'catalog' => 'OPT-IN Pages',
+            'description' => 'OPT-IN Pages'
+        ],[
+            'catalog' => 'Cold Phone Scripts',
+            'description' => 'Cold Phone Scripts'
+        ],[
+            'catalog' => 'Testimonials',
+            'description' => 'Testimonials'
+        ],];
 
-        Catalog::create($sales);
+        foreach ($sales as $sale) {
+            Catalog::create($sale);
+        }
     }
 }
